9f606c7ea9aebc6a3e34a6f9fd4d0aa07e04caa9
David J. Brenes <davidjbrenes@gmail.com>
2017-10-22 00:28:32 +0200

    Added the cropsize uploader

--- app/models/carrierwave/cropsize/image_crop_size.rb
+++ app/models/carrierwave/cropsize/image_crop_size.rb
@@ -1,6 +1,6 @@
 module Carrierwave::Cropsize
   class ImageCropSize < ApplicationRecord
     belongs_to :image_crop, inverse_of: :sizes
-
+    mount_uploader :crop, ImageCropSizeUploader
   end
 end
