2967d279781ac2ac0409ff765872067c599fb3cc
David J. Brenes <davidjbrenes@gmail.com>
2017-10-23 00:42:56 +0200

    Resizing the crop size when the file is uploaded

--- app/uploaders/carrierwave/cropsize/image_crop_size_uploader.rb
+++ app/uploaders/carrierwave/cropsize/image_crop_size_uploader.rb
@@ -1,5 +1,13 @@
 module Carrierwave::Cropsize
   class ImageCropSizeUploader < ApplicationUploader
 
+    process :resize_crop
+
+    def resize_crop
+      manipulate! do |img|
+        img.resize model.width
+      end
+    end
+
   end
 end
--- spec/uploaders/image_crop_size_uploader_spec.rb
+++ spec/uploaders/image_crop_size_uploader_spec.rb
@@ -6,7 +6,8 @@ describe Carrierwave::Cropsize::ImageCropSizeUploader do
     let(:image) { Carrierwave::Cropsize::Image.create! image: File.open(File.join(Carrierwave::Cropsize::Engine.root, image_path)) }
     let(:crop) { image.crops.create!(aspect_ratio: aspect_ratio, crop: image.image.file ) }
     let(:aspect_ratio) { "1:1" }
-    let(:crop_size) { crop.sizes.create! crop: image.image }
+    let(:crop_size) { crop.sizes.create! width: crop_width, crop: crop.crop.file  }
+    let(:crop_width) { 300 }
     let(:image_path) { "spec/uploaders/assets/horizontal.jpg" }
 
     context 'when creating a crop size' do
@@ -14,6 +15,11 @@ describe Carrierwave::Cropsize::ImageCropSizeUploader do
       it 'should upload the file' do
         expect(crop_size.crop.file.extension).to eq "jpg"
       end
+
+      it 'should change the size size' do
+        expect(::MiniMagick::Image.open(crop_size.crop.file.path)[:dimensions].first).to eq crop_width
+      end
+
     end
 
 
