5dc0d40d92775ea87850342a4a2c2249f3934848
David J. Brenes <davidjbrenes@gmail.com>
2017-10-31 02:50:11 +0100

    Usng the base64 uploader

--- app/models/carrierwave/cropsize/image_crop.rb
+++ app/models/carrierwave/cropsize/image_crop.rb
@@ -3,7 +3,7 @@ module Carrierwave::Cropsize
     belongs_to :image, inverse_of: :crops
     has_many :sizes, class_name: "Carrierwave::Cropsize::ImageCropSize", inverse_of: :image_crop
 
-    mount_uploader :crop, ImageCropUploader
+    mount_base64_uploader :crop, ImageCropUploader
 
     validates :aspect_ratio, presence: true
 
